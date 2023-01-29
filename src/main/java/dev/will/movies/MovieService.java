ff0c7b5b0dd4c31afcb4712b3f8bbe07cb4f763d
Will <wy427@nyu.edu>
2023-01-29 18:21:49 -0500

    added getMoviebyImdbId route

--- src/main/java/dev/will/movies/MovieService.java
+++ src/main/java/dev/will/movies/MovieService.java
@@ -1,15 +1,21 @@
 package dev.will.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
-    return movieRepository.findAll();
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
