186627f8c73cb5966816e8e82806d36c1799dc96
Will <wy427@nyu.edu>
2023-01-29 17:59:50 -0500

    getMovie route, repository and service setup

---
+++ src/main/java/dev/will/movies/MovieRepository.java
@@ -0,0 +1,11 @@
+package dev.will.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+
+}
